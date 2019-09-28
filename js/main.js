235110ac6ae7f8d79a28316afb5d42e0dec73c62
Adel <rizvanovaadel@gmail.com>
2019-09-28 18:15:56 +0300

    8. Личный проект: подробности

--- js/main.js
+++ js/main.js
@@ -1,5 +1,8 @@
 'use strict';
 
+var ESC_KEYCODE = 27;
+var ENTER_KEYCODE = 13;
+
 //  Мокки
 var TYPES = [
   'palace',
@@ -23,10 +26,24 @@ var PHOTOS = [
   'http://o0.github.io/assets/images/tokyo/hotel3.jpg',
 ];
 
+var ROOMS = [
+  'комната',
+  'комнаты',
+  'комнат',
+];
+
+var GUESTS = [
+  'гостя',
+  'гостей',
+  'гостей',
+];
+
 var POSITION_Y_MIN = 130;
 var POSITION_Y_MAX = 630;
 var PIN_WIDTH = 40;
 var PIN_HEIGHT = 40;
+var PHOTO_WIDTH = 45;
+var PHOTO_HEIGHT = 40;
 
 //  Функция перемешивания массива
 function shuffle(array) {
@@ -80,7 +97,7 @@ var getTranslateTypes = function (type) {
   }
   return translate;
 };
-
+window.onload = function() {
 // Иконка для features
 var fragmentFeatures = document.createDocumentFragment();
 var featuresPic = document.createElement('li');
@@ -88,25 +105,13 @@ var getFeaturesPic = function (array) {
   for (var i = 0; i < array.length; i++) {
     var pic = featuresPic.cloneNode(true);
     pic.classList.add('popup__feature');
-    if (array[i] === 'wifi') {
-      pic.classList.add('popup__feature--wifi');
-    } else if (array[i] === 'dishwasher') {
-      pic.classList.add('popup__feature--dishwasher');
-    } else if (array[i] === 'parking') {
-      pic.classList.add('popup__feature--parking');
-    } else if (array[i] === 'washer') {
-      pic.classList.add('popup__feature--washer');
-    } else if (array[i] === 'elevator') {
-      pic.classList.add('popup__feature--elevator');
-    } else {
-      pic.classList.add('popup__feature--conditioner');
-    }
+    pic.classList.add('popup__feature--' + array[i]);
     fragmentFeatures.appendChild(pic);
-  }
+    }
   return fragmentFeatures;
 };
 
-//  фотографии в карточке
+//  Фотографии в карточке
 var fragmentPhotos = document.createDocumentFragment();
 var photos = document.createElement('img');
 var getPhotos = function (array) {
@@ -114,12 +119,27 @@ var getPhotos = function (array) {
     var photo = photos.cloneNode(true);
     photo.src = array[i];
     photo.classList.add('popup__photo');
-    photo.width = '45';
-    photo.height = '40';
+    photo.width = PHOTO_WIDTH;
+    photo.height = PHOTO_HEIGHT;
     photo.alt = 'Фотография жилья';
     fragmentPhotos.appendChild(photo);
   }
   return fragmentPhotos;
+};
+
+//  Функция согласования существительных и чисел
+var connectNounAndNumbers = function (number, array) {
+  number = Math.abs(number) % 100;
+  var number1 = number % 10;
+  if (number > 10 && number < 20) {
+    return array[2];
+  } else if (number1 > 1 && number1 < 5) {
+    return array[1];
+  } else if (number1 === 1) {
+    return array[0];
+  } else {
+  return array[2];
+  };
 };
 
 var pins = [];
@@ -150,9 +170,6 @@ for (var i = 0; i < 8; i++) {
   };
 }
 
-//  2. У блока .map уберите класс .map--faded.
-document.querySelector('.map').classList.remove('.map--faded');
-
 //  3. На основе данных, созданных в первом пункте, создайте DOM-элементы, соответствующие меткам на карте, и заполните их данными из массива. Итоговую разметку метки .map__pin можно взять из шаблона #pin
 
 var similarPinsTemplate = document.querySelector('#pin').content.querySelector('.map__pin');
@@ -180,7 +197,7 @@ advertisementElement.querySelector('.popup__title').textContent = pins[0].offer.
 advertisementElement.querySelector('.popup__text--address').textContent = pins[0].offer.address;
 advertisementElement.querySelector('.popup__text--price').textContent = pins[0].offer.price + '₽/ночь';
 advertisementElement.querySelector('.popup__type').textContent = getTranslateTypes(pins[0].offer.type);
-advertisementElement.querySelector('.popup__text--capacity').textContent = pins[0].offer.rooms + ' комнаты для ' + pins[0].offer.guests + ' гостей';
+advertisementElement.querySelector('.popup__text--capacity').textContent = pins[0].offer.rooms + ' ' + connectNounAndNumbers(pins[0].offer.rooms, ROOMS) + ' для ' + pins[0].offer.guests + ' ' + connectNounAndNumbers(pins[0].offer.guests, GUESTS);
 advertisementElement.querySelector('.popup__text--time').textContent = 'Заезд после ' + pins[0].offer.checkin + ', выезд до ' + pins[0].offer.checkout;
 advertisementElement.querySelector('.popup__features').innerHTML = '';
 advertisementElement.querySelector('.popup__features').appendChild(getFeaturesPic(pins[0].offer.features));
@@ -193,4 +210,71 @@ fragmentAdds.appendChild(advertisementElement);
 
 var similarAddElement = document.querySelector('.map');
 similarAddElement.appendChild(fragmentAdds);
-document.querySelector('.map__filters-container').insertAdjacentHTML('beforebegin', similarAddElement);
+document.querySelector('.map').insertBefore(similarAddElement, 'map__filters-container');
+
+
+
+//  Добавить через DOM-операции fieldset атрибут disabled
+var fieldset = document.querySelectorAll('fieldset');
+for (var i = 0; i < fieldset.length; i++) {
+  fieldset[i].setAttribute('disabled', true);
+};
+
+//  Что с этим делать? Форма с фильтрами .map__filters заблокирована так же, как и форма .ad-form;
+
+var onPageActive = function () {
+  document.querySelector('.map').classList.remove('map--faded');
+  document.querySelector('.ad-form').classList.remove('ad-form--disabled');
+  for (var i = 0; i < fieldset.length; i++) {
+    fieldset[i].removeAttribute('disabled');
+  };
+};
+
+var onPageDisabled = function () {
+  document.querySelector('.map').classList.add('map--faded');
+  document.querySelector('.ad-form').classList.add('ad-form--disabled');
+  for (var i = 0; i < fieldset.length; i++) {
+    fieldset[i].setAttribute('disabled');
+  };
+};
+
+var mapPinMain = document.querySelector('map__pin--main');
+mapPinMain.addEventListener('mousedown', function () {
+  onPageActive();
+});
+
+mapPinMain.addEventListener('keydown', function (evt) {
+  if (evt.keyCode === ENTER_KEYCODE) {
+    onPageActive();
+  }
+  //  здесь должен находиться вызов метода, который устанавливает значения поля ввода адреса.
+});
+
+//  Находим координаты метки и подставляем их в поле Адрес, можно ли это сделать сразу в HTML?
+var address = document.querySelector('input[name = address]');
+address.removeAttribute('value');
+address.value = mapPinMain.style.left + '; ' + mapPinMain.style.top;
+
+//  3.5. Поля «Время заезда» и «Время выезда» синхронизированы: при изменении значения одного поля, во втором выделяется соответствующее ему. Например, если время заезда указано «после 14», то время выезда будет равно «до 14» и наоборот.
+
+var timeIn = document.querySelector('select[name="timein"]');
+var timeOut = document.querySelector('select[name="timeout"]');
+/*
+timeIn.addEventListener('click', function (evt) {
+  if () {
+    timeOut
+  }
+});
+*/
+
+var formReset = document.querySelector('.ad-form__reset');
+var onFormResetClick = formReset.addEventListener ('click', function () {
+  onPageDisabled();
+  fieldset.reset();
+
+//все заполненные поля возвращются в изначальное состояние, в том числе фильтры;
+//метки похожих объявлений и карточка активного объявления удаляются;
+//метка адреса возвращается в исходное положение;
+//значение поля адреса корректируется соответственно положению метки;
+});
+};
